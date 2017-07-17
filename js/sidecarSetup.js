3c6f9d4e3eea7e12356743d229b587d68f621b8c
Jeff Scott Ward <jsward.17@gmail.com>
2017-07-17 13:51:45 -0400

    publish: add deploy script
    generated from commit 315f6013b92031af1af171f5ded4696722619e2f

---
+++ js/sidecarSetup.js
@@ -0,0 +1,10 @@
+'use strict';
+
+((window.gitter = {}).chat = {}).options = {
+  room: 'uport-project/Lobby',
+  activationElement: false,
+  showChatByDefault: true,
+  useStyles: false,
+  preload: true,
+  targetElement: '.gitter .content'
+};
