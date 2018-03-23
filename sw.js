54f5e70b48e185b30094cd0aa4102946e15aeddb
localredhead <levi.strope@gmail.com>
2018-03-23 12:12:00 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-6b64689c4e2e4b5ffc8c.js","ba1e0457466f41a92d4c38178a78bb45"],["/commons-89ad3a3c609742de8014.js","fc7c43a2e49a2cff03d50de507cc2801"],["/index.html","6a09e8b2bcefa7de4c4387e295448245"],["/manifest.json","a9a58fc24742b97ffdccdac75f9928c7"],["/offline-plugin-app-shell-fallback/index.html","13edeb12c6bfbcbe4403166d7927efd5"]];
+var precacheConfig = [["/app-6b64689c4e2e4b5ffc8c.js","ba1e0457466f41a92d4c38178a78bb45"],["/commons-89ad3a3c609742de8014.js","fc7c43a2e49a2cff03d50de507cc2801"],["/index.html","fd08a858390d0908b1b00d6008014b8d"],["/manifest.json","a9a58fc24742b97ffdccdac75f9928c7"],["/offline-plugin-app-shell-fallback/index.html","2836b9f150dbcfd745672de9433233a5"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
