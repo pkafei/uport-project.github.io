6b2961eacee121cdc897175ebbc2d55ce7b7892d
gbugyis <greg@bugyis.com>
2018-03-26 12:38:58 +0300

    Using activeClassName for header link styling

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -20,6 +20,11 @@ const NavContainer = styled.div`
 
   }
 `
+const StyledLink = styled(Link)`
+  &.active {
+    text-decoration: underline
+  }
+`
 
 class Navigation extends React.Component {
   navListItems() {
@@ -29,7 +34,7 @@ class Navigation extends React.Component {
       if(this.props.data){
         this.props.data.forEach(page => {
           if((page.node.frontmatter.index === 0) && (page.node.frontmatter.category === section)) {
-            const link = (<Link className={`w-nav-link nav-link`} to={`${page.node.fields.slug}`} key={section}> {section.charAt(0).toUpperCase() + section.slice(1)} </Link>);
+            const link = (<StyledLink className={`w-nav-link nav-link`} to={`${page.node.fields.slug}`} activeStyle={'active'} key={section}> {section.charAt(0).toUpperCase() + section.slice(1)} </StyledLink>);
             switch(section){
               case "overview":
                 navItems[0] = link;
