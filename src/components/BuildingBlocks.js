72e536e69a7a58dcac846e4b577197e1190a2879
gbugyis <greg@bugyis.com>
2018-04-25 16:49:15 +0300

    Switching BuildingBlocks to a very simple flexbox grid

--- src/components/BuildingBlocks.js
+++ src/components/BuildingBlocks.js
@@ -2,31 +2,16 @@ import React, {Component} from 'react'
 import styled from 'styled-components'
 
 const Container = styled.section`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    max-width: 100%;
-    align-self: center;
     background-color: #fff;
-    padding: 60px 0;
-
+    .building-blocks-wrapper {
+      max-width: 70vw;
+      margin: 0 auto;
+      padding: 60px 0;
+    }
     h2 {
       font-size: 32px;
       font-weight: bold;
       line-height: 40px;
-    }
-
-    div:first-child {
-      max-width: 95vw;
-      margin: auto;
-    }
-
-    .building-blocks-left {
-      float:left;
-    } 
-    .building-blocks-right {
-      float: right;
     }
     h3, h4 {
       color: #8986A0;
@@ -60,64 +45,61 @@ const Container = styled.section`
         font-weight: normal;
         top: 7px;
         height: 19px;
-        width: 40px;
+        width: 70px;
         padding: 0px 15px 0px 16px;
         text-align: center;
         letter-spacing: 0.75px;
-        line-height: 22px;
+        line-height: 20px;
       }
     }
-
-    @media (max-width: 900px) {
-      .building-blocks-left {
-        float:none;
-      } 
-      .building-blocks-right {
-        float: none;
-    }
-  }
-
-
 `
 
 class BuildingBlocks extends Component {
   render () {
     return (
       <Container>
-        <div>
-          <h2>Identity Building Blocks</h2>
-          <div>
-            <div className={'building-blocks-left'}>
-              <h3>Libraries</h3>
+        <div className='building-blocks-wrapper'>
+          <div className={'Grid Grid--gutters'}>
+            <div className='Grid-cell'>
+              <h2>Identity Building Blocks</h2>
+            </div>
+          </div>
+          <div className={'Grid Grid--gutters'}>
+            <div className='Grid-cell'>
               <div>
-                <h4>uPort Connect</h4>
-                <p>Single sign-on and transaction signing for your client-side app</p>
-                <div className={'code-block'}>
-                  <p>npm -i uport-connect</p>
+                <h3>Libraries</h3>
+                <div>
+                  <h4>uPort Connect</h4>
+                  <p>Single sign-on and transaction signing for your client-side app</p>
+                  <div className={'code-block'}>
+                    <p>npm -i uport-connect</p>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <h4>uPort JS</h4>
-                <p>Rquest, sign, and issue credentials from your app server</p>
-                <div className={'code-block'}>
-                  <p>npm -i uport-js</p>
+                <div>
+                  <h4>uPort JS</h4>
+                  <p>Rquest, sign, and issue credentials from your app server</p>
+                  <div className={'code-block'}>
+                    <p>npm -i uport-js</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className={'building-blocks-right'}>
-              <h3>Tools</h3>
+            <div className='Grid-cell'>
               <div>
-                <h4>uPort JavaScript Client</h4>
-                <p>Single sign-on and transaction signing for your client-side app</p>
-                <div className={'code-block'}>
-                  <p>npm -i uport-js-client</p>
+                <h3>Tools</h3>
+                <div>
+                  <h4>uPort JavaScript Client</h4>
+                  <p>Single sign-on and transaction signing for your client-side app</p>
+                  <div className={'code-block'}>
+                    <p>npm -i uport-js-client</p>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <h4 className={'alpha'}>uPort Identity CLI</h4>
-                <p>Create and manage uPort identities from the command line</p>
-                <div className={'code-block'}>
-                  <p>npm -i uport-js-client</p>
+                <div>
+                  <h4 className={'alpha'}>uPort Identity CLI</h4>
+                  <p>Create and manage uPort identities from the command line</p>
+                  <div className={'code-block'}>
+                    <p>npm -i uport-js-client</p>
+                  </div>
                 </div>
               </div>
             </div>
