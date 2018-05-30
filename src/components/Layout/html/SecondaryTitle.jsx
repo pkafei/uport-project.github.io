da857ef0467571c8ebc09ad888d86cdd024b3285
gbugyis <greg@bugyis.com>
2018-05-31 01:03:06 +0300

    Handling for anchor link doubleByte characters

--- src/components/Layout/html/SecondaryTitle.jsx
+++ src/components/Layout/html/SecondaryTitle.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import _ from 'lodash'
+import { cleanDoubleByteChars } from '../../../helpers/cleanDoubleByteChars'
 
 class SecondaryTitle extends Component {
   render () {
+    const h2Text = this.props.children[1]
+    const svgAnchor = this.props.children[0].props.children[0]
     return (
-      <h2 id={this.props.id}>
-        {this.props.children}
+      <h2 id={cleanDoubleByteChars(_.kebabCase(h2Text))}>
+        <a href={'#' + cleanDoubleByteChars(_.kebabCase(h2Text))} aria-hidden='true' className='anchor'>
+          {svgAnchor}
+        </a>
+        {h2Text}
       </h2>
     )
   }
